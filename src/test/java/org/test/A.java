775d177ab1a78afc4a5fb96a020d2b78025611c5
shanthi2912 <shanthibindu29@gmail.com>
2020-05-18 18:18:55 +0530

    Update the code

--- src/test/java/org/test/A.java
+++ src/test/java/org/test/A.java
@@ -3,8 +3,8 @@ package org.test;
 public class A {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		System.out.println("welcome");
+		System.out.println("Bindu");
 	}
 
 }
